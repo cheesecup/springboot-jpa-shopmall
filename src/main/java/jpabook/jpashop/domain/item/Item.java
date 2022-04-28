ffb1b17181f965ea6edf711d21ef76455687317e
cheesecup <kdy8252@naver.com>
2022-04-28 15:41:22 +0900

    상품 서비스,레포지토리 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,7 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter @Setter
-public class Item {
+public abstract class Item {
 
     @Id @GeneratedValue
     @Column(name = "item_id")
@@ -25,6 +26,20 @@ public class Item {
     private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<Category>();
+    private List<Category> categories = new ArrayList<>();
 
+    /* ==비즈니스 로직== */
+    /* stock 증가 */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /* stock 감소 */
+    public void removeStock(int quantity) {
+        int resStock = this.stockQuantity - quantity;
+        if (resStock < 0 ) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = resStock;
+    }
 }
