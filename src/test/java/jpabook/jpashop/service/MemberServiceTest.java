66434eb039aa7d1d6f6a2bde640822ac0187f5b5
cheesecup <kdy8252@naver.com>
2022-04-28 15:22:15 +0900

    회원가입, 중복회원 테스트 코드 작성

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -3,16 +3,19 @@ package jpabook.jpashop.service;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Transactional
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class) // JUnit 실행시 스프링과 테스트 통합
+@SpringBootTest // Autowired 실행 하기위해
+@Transactional // 반복 가능한 테스트 지원, 각각의 테스트를 실행할 때마다 트랜잭션을 시작하고 테스트가 끝나면 강제로 롤백(테스트 코드이기 때문)
 public class MemberServiceTest {
 
     @Autowired
@@ -21,7 +24,11 @@ public class MemberServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    EntityManager em;
+
     @Test
+//    @Rollback(false) -> 실제로 DB에 데이터가 들어간다
     public void 회원가입() throws Exception {
         //Given
         Member member = new Member();
@@ -31,11 +38,33 @@ public class MemberServiceTest {
         Long saveId = memberService.join(member);
 
         //Then
-        Assertions.assertEquals(member, memberRepository.findOne(saveId));
+//        em.flush();
+        assertEquals(member, memberRepository.findOne(saveId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
+        //Given
+        Member member1 = new Member();
+        member1.setName("kim");
 
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //When
+        memberService.join(member1);
+        memberService.join(member2); // 예외 발생
+
+//        expected = IllegalStateException.class를 사용하면 아래 코드를 적지 않아도 된다.
+//        try {
+//            memberService.join(member2);
+//        } catch (IllegalStateException e) {
+//            return;
+//        }
+
+        //Then
+        /*정상적으로 테스트가 성공하면 fail()이 나오면 안된다.
+        * 테스트 실패시 fail()가 실행이 된다.*/
+        fail("예외가 발생해야 한다.");
     }
 }
